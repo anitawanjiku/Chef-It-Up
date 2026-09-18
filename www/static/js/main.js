6fa93010fa246a6a0da34ee7d3cc6129f4c3f88b
Anya B <barran5@furman.edu>
2026-09-18 15:12:25 -0400

    created searchBar() and onclick() JS functions

--- www/static/js/main.js
+++ www/static/js/main.js
@@ -1,1 +1,10 @@
 console.log("main.js loaded - edit me in www/static/js/main.js");
+
+
+
+
+
+function searchBar() {
+    let searchIcon = document.getElementsByClassName("searchbutton")[0]
+    searchIcon.outerHTML = '<input class="searchbar" type="text" placeholder="Search..."></input>'
+}
