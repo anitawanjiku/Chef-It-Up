75062674160e050cc2f120d768ff219f4bb495dd
Anya B <barran5@furman.edu>
2026-09-18 15:19:13 -0400

    edited main.js

--- www/static/js/main.js
+++ www/static/js/main.js
@@ -1,12 +1,11 @@
 console.log("main.js loaded - edit me in www/static/js/main.js");
 
 
-
-
-
 function searchBar() {
-    let searchIcon = document.getElementsByClassName("searchbutton")[0]
-    searchIcon.outerHTML = '<input class="searchbar" type="text" placeholder="Search..."></input>'
+    let searchIcon = document.getElementsByClassName("searchbutton")[0];
+    searchIcon.outerHTML = '<input class="searchbar" type="text" placeholder="Search..."></input>';
+}
+
 function hambugerMenu(document){
     var newElement = document.createElement('p');
     newElement.textContent = 'Hello, World!';
