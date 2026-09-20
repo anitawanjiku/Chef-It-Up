d4bc89559aca4a02a6ca65c678a851a62fedba12
wsinnott16 <wsinnott16@gmail.com>
2026-09-20 19:02:29 -0400

    dropdown continue

--- www/static/js/main.js
+++ www/static/js/main.js
@@ -6,8 +6,19 @@ function searchBar() {
     searchIcon.outerHTML = '<input class="searchbar" type="text" placeholder="Search..."></input>';
 }
 
-function hambugerMenu(document){
-    var newElement = document.createElement('p');
-    newElement.textContent = 'Hello, World!';
-    document.getElementByID('HBButton').appendChild(newElement);
+function myFunction(){
+    document.getElementById("myDropdown").classList.toggle("show");
+}
+
+window.onclick = function(event){
+    if(!event.target.matches('.dropbtn')){
+        var dropdowns = this.document.getElementsByClassName("hamburger");
+        var i;
+        for(i = 0; i < dropdowns.length; i++){
+            var openDropdown = downdowns[i];
+            if(openDropdown.classList.contains('show')){
+                openDropdown.classList.remove('show');
+            }
+        }
+    }
 }
