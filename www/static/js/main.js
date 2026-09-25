0d0e31ec884c9c91f54cd3af6f945cd61e2f459b
Nicolas Cepeda <cepeni3@furman.edu>
2026-09-25 15:36:35 -0400

    changes

--- www/static/js/main.js
+++ www/static/js/main.js
@@ -23,7 +23,7 @@ window.onclick = function(event) {
     if (!event.target.matches('.dropbtn')) {
         let dropdowns = document.getElementsByClassName("dropdown-content");
         for (let i = 0; i < dropdowns.length; i++) {
-            var openDropdown = dropdowns[i];
+            var openDropdown = downdowns[i];
             if (openDropdown.classList.contains('show')) {
                 openDropdown.classList.remove('show');
             }
